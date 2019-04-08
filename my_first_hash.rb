6b81bf0d7d4dec5c7c903330aebc8a8dc0d93c05
Daniel Bovington <d.bovington@gmail.com>
2019-04-08 07:28:00 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,7 +1,7 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
 your_hash = {
-  "name" => Dan The Man
+  "name" => "Dan The Man"
   "Age" => 33
 }
   
@@ -50,5 +50,5 @@ shipping_manifest["gun powder"] = 4
 
   # return the shipping_manifest hash below
 
-shipping_manifest
+shipping_manifest [oil paintings]
 end
